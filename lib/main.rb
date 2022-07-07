c0f57a936c37ce064eedf8e59731cc5b077a1f5b
Rashika <agarwalrashika00@gmail.com>
2022-07-07 13:52:55 +0530

    made ReadAndWrite class

--- lib/main.rb
+++ lib/main.rb
@@ -1,18 +1,9 @@
 require 'csv'
 require './employee.rb'
-require './employee_list.rb'
+require './read_and_write.rb'
 
-employee_list = EmployeeList.make_employee_list('../bin/csv_file.csv')
-
-employee_list.sort_by!(&:designation)
-employee_hash = employee_list.group_by(&:designation)
-
-File.open('../bin/output2.txt',"w") do |fileptr|
-    employee_hash.each do |(designation,employee)|
-        fileptr.puts "#{designation}#{'s' if employee.length>1}"
-        employee.each {|each_employee|
-            fileptr.puts "#{each_employee.name} (EmpId: #{each_employee.empid})"
-        }
-        fileptr.puts
-    end
-end
+ReadAndWrite.read_csv('../bin/csv_file.csv')
+Employee.sort_by_designation
+employee_hash = Employee.group_by_designation
+ 
+ReadAndWrite.write_to_file('../bin/output2.txt', employee_hash)
